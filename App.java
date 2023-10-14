dcbea5fcee724a59df9416453d8551313f337da4
Chuntinne <107016351+Chuntinne@users.noreply.github.com>
2023-10-14 23:41:18 +0700

    Nhập dữ liệu từ người dùng

--- App.java
+++ App.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        System.out.println("Enter n: ");
+        int n = input.nextInt();
+        System.out.println("So ban da nhap la: " +n);
     }
 }
