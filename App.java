af8fb3afd6a974dc44fd5e3e8b32e23407daeae2
Chuntinne <107016351+Chuntinne@users.noreply.github.com>
2023-10-16 14:30:13 +0700

    Phép so sánh đúng hoặc sai

--- App.java
+++ App.java
@@ -1,11 +1,20 @@
 import java.util.Scanner;
 
 public class App {
-
-    public static void main(String[] args) {
-        Scanner input= new Scanner(System.in);
-        System.out.println("Enter n: ");
-        int n = input.nextInt();
-        System.out.println("So ban da nhap la: " +n);
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		System.out.println("Nhap a =");
+		a = sc.nextInt();
+		System.out.println("Nhap b =");
+		b = sc.nextInt();
+		
+		// Xuat ket qua so sanh
+		System.out.println(a +" == "+b +" : "+ (a==b));
+		System.out.println(a +" != "+b +" : "+ (a!=b));
+		System.out.println(a +" >= "+b +" : "+ (a>=b));
+		System.out.println("-----");
+		System.out.println("Ca hai so la so chan: "+ (a%2==0 && b%2==0));
+		System.out.println("Co it nhat 1 so chan: "+ (a%2==0 || b%2==0));
+	}
 }
